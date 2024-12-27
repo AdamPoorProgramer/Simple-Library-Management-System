d99e550ef952f6e24d596f052adf50a41055f835
AdamPoorProgramer <amiralishajari027@gmail.com>
2024-12-27 12:14:24 +0330

    Add logging functionality

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,15 +3,16 @@ package router
 import (
 	"LIBRARY-API-SERVER/api/model"
 	"LIBRARY-API-SERVER/internal/handler"
+	"LIBRARY-API-SERVER/pkg/logger"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupRoutes(db *gorm.DB, router *gin.Engine) {
+func SetupRoutes(db *gorm.DB, router *gin.Engine, log *logger.Logger) {
 	apiRouter := router.Group("/api")
-	handler.NewHandler[model.Book](db).Register(apiRouter)
-	handler.NewHandler[model.Member](db).Register(apiRouter)
-	handler.NewHandler[model.Borrowing](db).Register(apiRouter)
-	handler.NewHandler[model.Category](db).Register(apiRouter)
-	handler.NewHandler[model.BookCategory](db).Register(apiRouter)
+	handler.NewHandler[model.Book](db, log).Register(apiRouter, log)
+	handler.NewHandler[model.Member](db, log).Register(apiRouter, log)
+	handler.NewHandler[model.Borrowing](db, log).Register(apiRouter, log)
+	handler.NewHandler[model.Category](db, log).Register(apiRouter, log)
+	handler.NewHandler[model.BookCategory](db, log).Register(apiRouter, log)
 }
